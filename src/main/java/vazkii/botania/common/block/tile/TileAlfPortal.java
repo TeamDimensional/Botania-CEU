72192e16e65b9b9c9312caa63931d36a6afe54c8
BuildTools <unconfigured@null.spigotmc.org>
2026-02-24 08:54:10 +0000

    Removed random commented code that has been deprecated for a while now by locatePylons

--- src/main/java/vazkii/botania/common/block/tile/TileAlfPortal.java
+++ src/main/java/vazkii/botania/common/block/tile/TileAlfPortal.java
@@ -165,10 +165,6 @@ public class TileAlfPortal extends TileMod implements ITickable {
 			mb.addComponent(l.up(), ModBlocks.livingwood.getDefaultState());
 		for(BlockPos g : GLIMMERING_LIVINGWOOD_POSITIONS)
 			mb.addComponent(g.up(), ModBlocks.livingwood.getDefaultState().withProperty(BotaniaStateProps.LIVINGWOOD_VARIANT, LivingWoodVariant.GLIMMERING));
-		//		for(BlockPos p : PYLON_POSITIONS)
-		//			mb.addComponent(new BlockPos(-p.getX(), p.getY() + 1, -p.getZ()), ModBlocks.pylon.getDefaultState().withProperty(BotaniaStateProps.PYLON_VARIANT, PylonVariant.NATURA));
-		//		for(BlockPos p : POOL_POSITIONS)
-		//			mb.addComponent(new StateInsensitiveComponent(new BlockPos(-p.getX(), p.getY() + 1, -p.getZ()), ModBlocks.pool));
 
 		mb.addComponent(new BlockPos(0, 1, 0), ModBlocks.alfPortal.getDefaultState());
 		mb.setRenderOffset(new BlockPos(0, -1, 0));
