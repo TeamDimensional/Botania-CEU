b413f11edb054ba640f7c986a8d8a1ab2f0d3cc6
BuildTools <unconfigured@null.spigotmc.org>
2026-03-03 22:17:54 +0000

    Removed test recipe

--- src/main/java/vazkii/botania/common/crafting/ModElvenTradeRecipes.java
+++ src/main/java/vazkii/botania/common/crafting/ModElvenTradeRecipes.java
@@ -22,12 +22,9 @@ public class ModElvenTradeRecipes {
 	public static List<RecipeElvenTrade> dragonstoneRecipes;
 	public static RecipeElvenTrade elvenQuartzRecipe;
 	public static RecipeElvenTrade alfglassRecipe;
-	public static RecipeElvenTrade test;
 
 	public static void init() {
 		dreamwoodRecipe = BotaniaAPI.registerElvenTradeRecipe(new ItemStack(ModBlocks.dreamwood), LibOreDict.LIVING_WOOD);
-
-		test = BotaniaAPI.registerElvenTradeRecipe(new ItemStack(Items.DIAMOND), new ItemStack(Item.getItemById(12)), LibOreDict.DYE_WILDCARD);
 
 		elementiumRecipes = new ArrayList<>();
 		elementiumRecipes.add(BotaniaAPI.registerElvenTradeRecipe(new ItemStack(ModItems.manaResource, 1, 7), LibOreDict.MANA_STEEL, LibOreDict.MANA_STEEL));
